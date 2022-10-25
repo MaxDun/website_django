0f0cd42002f6279045ef3ad227dfcbee4ce3c5b3
MaxDun <maxdun2705@gmail.com>
2022-10-26 00:04:05 +0300

    first commit

---
+++ women/views.py
@@ -0,0 +1,87 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
+from .forms import *
+from .models import *
+
+menu = [{'title': 'Про сайт', 'url_name': 'about'},
+        {'title': 'Додати статтю', 'url_name': 'add_page'},
+        {'title': "Зворотній зв'язок", 'url_name': 'contact'},
+        {'title': 'Зайти', 'url_name': 'login'}
+        ]
+
+
+class WomenHome(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Головна сторінка'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Women.objects.filter(is_published=True)
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'Про сайт'})
+
+
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Додавання статті'
+        context['menu'] = menu
+        return context
+
+
+def contact(request):
+    return HttpResponse("Зворотній зв'язок")
+
+
+def login(request):
+    return HttpResponse('Авторизація')
+
+
+class ShowPost(DetailView):
+    model = Women
+    template_name = 'women/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
+
+
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категорія - ' + str(context['posts'][0].cat)
+        context['menu'] = menu
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Сторінка не знайдена</h1>')
